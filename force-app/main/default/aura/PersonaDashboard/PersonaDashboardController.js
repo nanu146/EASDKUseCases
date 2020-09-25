652a5ef0641e54251e15657acc59e7b11d43f56a
Bharadwaj Kopparthi <pradeep146nanu@gmail.com>
2020-09-25 17:48:22 +0530

    persona dashboard changes

--- force-app/main/default/aura/PersonaDashboard/PersonaDashboardController.js
+++ force-app/main/default/aura/PersonaDashboard/PersonaDashboardController.js
@@ -1,5 +1,9 @@
 ({
   doInit: function (component, event, helper) {
     helper.getPersonaDashboardDetails(component);
+  },
+  selectionChange: function (component, event, helper) {
+    var selectedOptionValue = event.getParam("value");
+    component.set("v.dashboardId", selectedOptionValue);
   }
 });
--- force-app/main/default/aura/PersonaDashboard/PersonaDashboardHelper.js
+++ force-app/main/default/aura/PersonaDashboard/PersonaDashboardHelper.js
@@ -1,14 +1,52 @@
 ({
-    getPersonaDashboardDetails : function(component){
-        var getPersonaDashboards = component.get("c.getPersonaDashboards");
-        getPersonaDashboards.setCallback(this,function(response){
-            var status = response.getState();
-            if(status === "SUCCESS"){
-                console.log(response.getReturnValue());
+  getPersonaDashboardDetails: function (component) {
+    var getPersonaDashboards = component.get("c.getPersonaDashboards");
+    var self = this;
+    getPersonaDashboards.setCallback(this, function (response) {
+      var status = response.getState();
+      if (status === "SUCCESS") {
+        console.log(response.getReturnValue());
+        self.getEADashboards(component, response.getReturnValue());
+      }
+    });
+
+    $A.enqueueAction(getPersonaDashboards);
+  },
+  getEADashboards: function (component, profileDashboards) {
+    var context = {apiVersion: "49"};
+    var methodName = "listDashboards";
+    var methodParameters = {};
+    var self = this;
+    var sdk = component.find("sdk");
+
+    sdk.invokeMethod(
+      context,
+      methodName,
+      methodParameters,
+      $A.getCallback(function (err, data) {
+        if (err !== null) {
+          //DO THIS IF THE METHOD FAILS
+          console.error("SDK error", err);
+        } else {
+            var options = self.datasetToOption(data.dashboards, profileDashboards)
+            // set options to combobax
+            component.set("v.options",options)
+            // set first option dashboard as default
+            component.set("v.dashboardId",(options && options.length > 0) ? options[0].value: null)
+        }
+      })
+    );
+  },
+  datasetToOption: function(dashboards, profileDashboards){
+    var options = []
+
+    profileDashboards.forEach(function(d){
+        dashboards.forEach(function(c){
+            if(c.id == d.DashboardId__c){
+                options.push({"value": c.id, "label": c.label})
             }
-
-        });
-
-        $A.enqueueAction(getPersonaDashboards);
-    }
-})
+        })
+    })
+    return options;
+  }
+});

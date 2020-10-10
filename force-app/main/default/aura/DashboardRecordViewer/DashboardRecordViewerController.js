c22ea944fdfce80a9dd27a47c6007717c097b303
Bharadwaj Kopparthi <pradeep146nanu@gmail.com>
2020-10-11 00:16:42 +0530

    selection update demo

--- force-app/main/default/aura/DashboardRecordViewer/DashboardRecordViewerController.js
+++ force-app/main/default/aura/DashboardRecordViewer/DashboardRecordViewerController.js
@@ -1,5 +1,15 @@
 ({
   handleSelectionChanged: function (component, event, helper) {
     console.log("selection changed in the dashboard");
+    var params = event.getParams();
+    var payload = params.payload;
+    if (payload) {
+      var step = payload.step;
+      var data = payload.data;
+      if(payload.data[0].hasOwnProperty("Id") || payload.data[0].hasOwnProperty("AccountId")){
+        var id =  payload.data[0].hasOwnProperty("Id") ? payload.data[0]["Id"] : payload.data[0]["AccountId"];
+        component.set("v.recordId",id);
+      }
+    }
   }
 });
